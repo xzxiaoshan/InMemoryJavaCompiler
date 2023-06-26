429e0d56a865142c0ca95b0e1033f2cded48ce75
shanhy <xz.xiaoshan@gmail.com>
2023-06-26 09:51:03 +0800

    1、aClass.newInstance()  ----->    aClass.getDeclaredConstructor().newInstance() 2、srcResStream 流关闭和判空 3、代码换行

--- src/test/java/org/mdkt/compiler/InMemoryJavaCompilerTest.java
+++ src/test/java/org/mdkt/compiler/InMemoryJavaCompilerTest.java
@@ -14,87 +14,97 @@ import java.util.List;
 import java.util.Map;
 
 public class InMemoryJavaCompilerTest {
-	private static final Logger logger = LoggerFactory.getLogger(InMemoryJavaCompilerTest.class);
+    private static final Logger logger = LoggerFactory.getLogger(InMemoryJavaCompilerTest.class);
 
-	@Rule
-	public ExpectedException thrown = ExpectedException.none();
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
-	public String getResourceAsString(String path) throws Exception {
-		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-		InputStream srcResStream = classloader.getResourceAsStream(path);
+    public String getResourceAsString(String path) throws Exception {
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        try (InputStream srcResStream = classloader.getResourceAsStream(path)) {
 
-		// Thanks to https://www.baeldung.com/convert-input-stream-to-string
-		ByteArrayOutputStream buf = new ByteArrayOutputStream();
-		byte[] chunk = new byte[4096];
-		int bytesRead = 0;
-		while ((bytesRead = srcResStream.read(chunk, 0, chunk.length)) != -1) {
-			buf.write(chunk, 0, bytesRead);
-		}
-		return new String(buf.toByteArray(), StandardCharsets.UTF_8);
-	}
+            // Thanks to https://www.baeldung.com/convert-input-stream-to-string
+            ByteArrayOutputStream buf = new ByteArrayOutputStream();
+            byte[] chunk = new byte[4096];
+            int bytesRead = 0;
+            while (srcResStream != null && (bytesRead = srcResStream.read(chunk, 0, chunk.length)) != -1) {
+                buf.write(chunk, 0, bytesRead);
+            }
+            return new String(buf.toByteArray(), StandardCharsets.UTF_8);
+        }
+    }
 
-	@Test
-	public void compile_WhenTypical() throws Exception {
-		Class<?> helloClass = InMemoryJavaCompiler.newInstance().compile("org.mdkt.HelloClass", getResourceAsString("compile_WhenTypical/HelloClass.java"));
-		Assert.assertNotNull(helloClass);
-		Assert.assertEquals(1, helloClass.getDeclaredMethods().length);
-	}
+    @Test
+    public void compile_WhenTypical() throws Exception {
+        Class<?> helloClass = InMemoryJavaCompiler.newInstance()
+                .compile("org.mdkt.HelloClass", getResourceAsString("compile_WhenTypical/HelloClass.java"));
+        Assert.assertNotNull(helloClass);
+        Assert.assertEquals(1, helloClass.getDeclaredMethods().length);
+    }
 
-	@Test
-	public void compileAll_WhenTypical() throws Exception {
-		Map<String, Class<?>> compiled = InMemoryJavaCompiler.newInstance().addSource("A", getResourceAsString("compileAll_WhenTypical/A.java")).addSource("B", getResourceAsString("compileAll_WhenTypical/B.java")).compileAll();
+    @Test
+    public void compileAll_WhenTypical() throws Exception {
+        Map<String, Class<?>> compiled = InMemoryJavaCompiler.newInstance()
+                .addSource("A", getResourceAsString("compileAll_WhenTypical/A.java"))
+                .addSource("B", getResourceAsString("compileAll_WhenTypical/B.java"))
+                .compileAll();
 
-		Assert.assertNotNull(compiled.get("A"));
-		Assert.assertNotNull(compiled.get("B"));
-		Assert.assertNotNull(compiled.get("C"));
+        Assert.assertNotNull(compiled.get("A"));
+        Assert.assertNotNull(compiled.get("B"));
+        Assert.assertNotNull(compiled.get("C"));
 
-		Class<?> aClass = compiled.get("A");
-		Object a = aClass.newInstance();
-		Assert.assertEquals("B!", aClass.getMethod("b").invoke(a).toString());
+        Class<?> aClass = compiled.get("A");
+        Object a = aClass.getDeclaredConstructor().newInstance();
+        Assert.assertEquals("B!", aClass.getMethod("b").invoke(a).toString());
 
-		Class<?> cClass = compiled.get("C");
-		Object c = cClass.newInstance();
-		String helloInvokeResult = cClass.getMethod("hello").invoke(c).toString();
-		System.out.println("helloInvokeResult >>> " + helloInvokeResult);
-		Assert.assertEquals("Hello Shanhy", helloInvokeResult);
-	}
+        Class<?> cClass = compiled.get("C");
+        Object c = cClass.getDeclaredConstructor().newInstance();
+        String helloInvokeResult = cClass.getMethod("hello").invoke(c).toString();
+        System.out.println("helloInvokeResult >>> " + helloInvokeResult);
+        Assert.assertEquals("Hello Shanhy", helloInvokeResult);
+    }
 
-	@Test
-	public void compile_WhenSourceContainsInnerClasses() throws Exception {
-		Class<?> helloClass = InMemoryJavaCompiler.newInstance().compile("org.mdkt.HelloClass", getResourceAsString("compile_WhenSourceContainsInnerClasses/HelloClass.java"));
-		Assert.assertNotNull(helloClass);
-		Assert.assertEquals(1, helloClass.getDeclaredMethods().length);
-	}
+    @Test
+    public void compile_WhenSourceContainsInnerClasses() throws Exception {
+        Class<?> helloClass = InMemoryJavaCompiler.newInstance()
+                .compile("org.mdkt.HelloClass", getResourceAsString("compile_WhenSourceContainsInnerClasses/HelloClass.java"));
+        Assert.assertNotNull(helloClass);
+        Assert.assertEquals(1, helloClass.getDeclaredMethods().length);
+    }
 
-	@Test
-	public void compile_whenError() throws Exception {
-		thrown.expect(CompilationException.class);
-		thrown.expectMessage("Unable to compile the source");
-		InMemoryJavaCompiler.newInstance().compile("org.mdkt.HelloClass", getResourceAsString("compile_whenError/HelloClass.java"));
-	}
+    @Test
+    public void compile_whenError() throws Exception {
+        thrown.expect(CompilationException.class);
+        thrown.expectMessage("Unable to compile the source");
+        InMemoryJavaCompiler.newInstance()
+                .compile("org.mdkt.HelloClass", getResourceAsString("compile_whenError/HelloClass.java"));
+    }
 
-	@Test
-	public void compile_WhenFailOnWarnings() throws Exception {
-		thrown.expect(CompilationException.class);
-		InMemoryJavaCompiler.newInstance().compile("org.mdkt.HelloClass", getResourceAsString("compile_WhenFailOnWarnings/HelloClass.java"));
-	}
+    @Test
+    public void compile_WhenFailOnWarnings() throws Exception {
+        thrown.expect(CompilationException.class);
+        InMemoryJavaCompiler.newInstance()
+                .compile("org.mdkt.HelloClass", getResourceAsString("compile_WhenFailOnWarnings/HelloClass.java"));
+    }
 
-	@Test
-	public void compile_WhenIgnoreWarnings() throws Exception {
-		Class<?> helloClass = InMemoryJavaCompiler.newInstance().ignoreWarnings().compile("org.mdkt.HelloClass", getResourceAsString("compile_WhenIgnoreWarnings/HelloClass.java"));
-		List<?> res = (List<?>) helloClass.getMethod("hello").invoke(helloClass.newInstance());
-		Assert.assertEquals(0, res.size());
-	}
+    @Test
+    public void compile_WhenIgnoreWarnings() throws Exception {
+        Class<?> helloClass = InMemoryJavaCompiler.newInstance().ignoreWarnings()
+                .compile("org.mdkt.HelloClass", getResourceAsString("compile_WhenIgnoreWarnings/HelloClass.java"));
+        List<?> res = (List<?>) helloClass.getMethod("hello").invoke(helloClass.getDeclaredConstructor().newInstance());
+        Assert.assertEquals(0, res.size());
+    }
 
-	@Test
-	public void compile_WhenWarningsAndErrors() throws Exception {
-		thrown.expect(CompilationException.class);
-		try {
-			InMemoryJavaCompiler.newInstance().compile("org.mdkt.HelloClass", getResourceAsString("compile_WhenWarningsAndErrors/HelloClass.java"));
-		} catch (Exception e) {
-			logger.info("Exception caught: {}", e);
-			throw e;
-		}
-	}
+    @Test
+    public void compile_WhenWarningsAndErrors() throws Exception {
+        thrown.expect(CompilationException.class);
+        try {
+            InMemoryJavaCompiler.newInstance()
+                    .compile("org.mdkt.HelloClass", getResourceAsString("compile_WhenWarningsAndErrors/HelloClass.java"));
+        } catch (Exception e) {
+            logger.info("Exception caught: {}", e.getMessage());
+            throw e;
+        }
+    }
 
 }
